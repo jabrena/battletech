76b070121333a169a56b99979bc8057606783d43
Rich Hildebrand <richhildebrand@gmail.com>
2013-11-12 22:09:17 -0500

    select each hex

--- map/drawHex.js
+++ map/drawHex.js
@@ -2,31 +2,33 @@ var values = {
 	amount: 3
 };
 
-var group = new Group();
 
-var hexagon = new Path.RegularPolygon({
-	center: view.center,
-	sides: 6,
-	radius: 75,
-	fillColor: 'darkgrey',
-	parent: group
-});
+var createHex = function() {
+	var group = new Group();
 
-group.remove();
+	var hexagon = new Path.RegularPolygon({
+		center: view.center,
+		sides: 6,
+		radius: 75,
+		fillColor: 'darkgrey',
+		parent: group
+	});
+
+	return hexagon;
+}
 
 
-group = new Group();
-var size = hexagon.bounds.size;
+var size = createHex().bounds.size;
 for (var y = 0; y < values.amount; y++) {
 	for (var x = 0; x < values.amount; x++) {
-		var copy = hexagon.clone();
-		copy.position += size * [x + (y % 2 ? 0.5 : 0), y * 0.75];
-		group.addChild(copy);
+		var hex = createHex();
+		hex.position += size * [x + (y % 2 ? 0.5 : 0), y * 0.75];
 	}
 }
 
-function onMouseMove(event) {
+var onMouseMove = function(event) {
 	project.activeLayer.selected = false;
 	if (event.item)
 		event.item.selected = true;
 }
+
