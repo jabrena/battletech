e119a4dd3272d410fc6a967eb1f4132ebdf66db1
Rich Hildebrand <richhildebrand@gmail.com>
2013-11-12 21:49:04 -0500

    drawing several hex!

--- map/drawHex.js
+++ map/drawHex.js
@@ -1,8 +1,26 @@
+var values = {
+	amount: 3
+};
+
 var group = new Group();
+
 var hexagon = new Path.RegularPolygon({
 	center: view.center,
 	sides: 6,
-	radius: 50,
+	radius: 75,
 	fillColor: 'gray',
 	parent: group
 });
+
+group.remove();
+
+
+group = new Group();
+var size = hexagon.bounds.size;
+for (var y = 0; y < values.amount; y++) {
+	for (var x = 0; x < values.amount; x++) {
+		var copy = hexagon.clone();
+		copy.position += size * [x + (y % 2 ? 0.5 : 0), y * 0.75];
+		group.addChild(copy);
+	}
+}
