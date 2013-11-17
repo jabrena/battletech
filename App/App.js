3ced3dcc8534e9f0f17a8d2d8754347f1c93dd18
Rich <richhildebrand@gmail.com>
2013-11-17 12:42:28 -0500

    creating pathfinder grid

--- App/App.js
+++ App/App.js
@@ -1,4 +1,9 @@
 define(['PathFinding/Core/Grid', 'PathFinding/Finders/AStarFinder'], 
 function(Grid, PathFinder) {
+	var map = {
+		width: 9,
+		height: 5
+	}
 
+	var grid = new Grid(map.width, map.height); 
 });
