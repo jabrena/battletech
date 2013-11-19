fd6e1d3103f56133e3d0f9cb3c5bd45fc73a9596
Rich <rich.hildebrand@gmail.com>
2013-11-18 23:59:43 -0500

    draw map without paper script

--- App/App.js
+++ App/App.js
@@ -4,22 +4,22 @@ function(Grid, PathFinder, MapDrawer) {
 
 	var SetUpCanvas = function() {
 		$(document).ready(function() {
-			var canvas = $('#myCanvas');
+			var canvas = $('#myCanvas')[0];
 			paper.setup(canvas);
+
+			var map = {
+				width: 9,
+				height: 5
+			}
+
+			var grid = new Grid(map.width, map.height); 
+			var pathFinder = new PathFinder();
+			var mapDrawer = new MapDrawer(grid)
+			mapDrawer.drawMap();
+
+			var gridBackup = grid.clone();
+			var path = pathFinder.findPath(1, 2, 4, 2, gridBackup);
+			console.log(path);
 		});
 	}();
-
-	var map = {
-		width: 9,
-		height: 5
-	}
-
-	var grid = new Grid(map.width, map.height); 
-	var pathFinder = new PathFinder();
-	var mapDrawer = new MapDrawer()
-	mapDrawer.drawMap();
-
-	var gridBackup = grid.clone();
-	var path = pathFinder.findPath(1, 2, 4, 2, gridBackup);
-	console.log(path);
 });
--- App/Map/MapDrawer.js
+++ App/Map/MapDrawer.js
@@ -1,14 +1,42 @@
 define(['PathFinding/Core/Grid'], function(Grid) {
 	'use strict';
+	var HEX_RADIUS = 75;
+	var TOP_LEFT_POINT = new paper.Point(HEX_RADIUS, HEX_RADIUS);
 
 	var _grid;
+
+	var _createHex = function(centerPosition) {
+		var group = new paper.Group();
+
+		var hexagon = new paper.Path.RegularPolygon({
+			center: centerPosition,
+			sides: 6,
+			radius: HEX_RADIUS,
+			fillColor: 'darkgrey',
+			parent: group
+		});
+
+		return hexagon;
+	}
 
 	var MapDrawer = function(grid) {
 		_grid = grid;
 	}
 
 	MapDrawer.prototype.drawMap = function()  {
-		console.log('draw map');
+		var tempHex = _createHex()
+		var size = tempHex.bounds.size;
+		tempHex.remove();
+		for (var y = 0; y < _grid.height; y++) {
+			for (var x = 0; x < _grid.width; x++) {
+				//hex.position += size * [x + (y % 2 ? 0.5 : 0), y * 0.75];
+				var startingPosition = new paper.Point(TOP_LEFT_POINT);
+				startingPosition.x += size._width * (x + (y % 2 ? 0.5 : 0));
+				startingPosition.y += size._height *(y * 0.75);
+				console.log(startingPosition);
+				var hex = _createHex(startingPosition);
+			}
+		}
 	}
 
 	return MapDrawer;
