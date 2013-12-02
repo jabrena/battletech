5a1ea28a8024053c1f3e31d98f2c6d7e3f80b2e7
Rich <rich.hildebrand@gmail.com>
2013-12-01 22:18:13 -0500

    view port now draws right / bottom smoother

--- App/Map/ViewPort.js
+++ App/Map/ViewPort.js
@@ -14,18 +14,15 @@ define(['AppGlobals'], function(appGlobals) {
 		var firstColumn = (numberOfHiddenColumns === 0) ? numberOfHiddenColumns : numberOfHiddenColumns - 1;
 
 		var numberOfColumnsToShow = Math.ceil(currentView.width / mapDetails.hexSize.width);
-		var lastColumn = numberOfHiddenColumns + numberOfColumnsToShow;
+		var lastColumn = numberOfHiddenColumns + numberOfColumnsToShow + 1;
 		lastColumn = (lastColumn > mapDetails.width) ? mapDetails.width : lastColumn;
 		
 		var numberOfHiddenRows = Math.floor(currentView.y / (mapDetails.hexSize.height * 0.75));
 		var numberOfRowsToShow = Math.ceil(currentView.height / (mapDetails.hexSize.height * 0.75));
 		var firstRow = (numberOfHiddenRows === 0) ? numberOfHiddenRows : numberOfHiddenRows - 1;
 		
-		var lastRow = numberOfHiddenRows + numberOfRowsToShow;
+		var lastRow = numberOfHiddenRows + numberOfRowsToShow + 1;
 		lastRow = (lastRow > mapDetails.height) ? mapDetails.height : lastRow;
-
-
-
 
 		return { firstColumn: firstColumn,
 				 lastColumn: lastColumn,
