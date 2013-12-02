838209750cc0baa7bfa1928cfc835ad736f42134
Rich <rich.hildebrand@gmail.com>
2013-12-01 22:07:12 -0500

    viewPort is now full when possible

--- App/Map/ViewPort.js
+++ App/Map/ViewPort.js
@@ -11,21 +11,26 @@ define(['AppGlobals'], function(appGlobals) {
 		var mapDetails = _mapDetails;
 
 		var numberOfHiddenColumns = Math.floor(currentView.x / mapDetails.hexSize.width);
-		var numberOfHiddenRows = Math.floor(currentView.y / (mapDetails.hexSize.height * 0.75));
+		var firstColumn = (numberOfHiddenColumns === 0) ? numberOfHiddenColumns : numberOfHiddenColumns - 1;
 
 		var numberOfColumnsToShow = Math.ceil(currentView.width / mapDetails.hexSize.width);
-		var numberOfRowsToShow = Math.ceil(currentView.height / (mapDetails.hexSize.height * 0.75));
-
 		var lastColumn = numberOfHiddenColumns + numberOfColumnsToShow;
 		lastColumn = (lastColumn > mapDetails.width) ? mapDetails.width : lastColumn;
-
+		
+		var numberOfHiddenRows = Math.floor(currentView.y / (mapDetails.hexSize.height * 0.75));
+		var numberOfRowsToShow = Math.ceil(currentView.height / (mapDetails.hexSize.height * 0.75));
+		var firstRow = (numberOfHiddenRows === 0) ? numberOfHiddenRows : numberOfHiddenRows - 1;
+		
 		var lastRow = numberOfHiddenRows + numberOfRowsToShow;
 		lastRow = (lastRow > mapDetails.height) ? mapDetails.height : lastRow;
 
-		return { firstColumn: numberOfHiddenColumns,
+
+
+
+		return { firstColumn: firstColumn,
 				 lastColumn: lastColumn,
 				 
-				 firstRow: numberOfHiddenRows,
+				 firstRow: firstRow,
 				 lastRow: lastRow
 			   };
 	}
