41e053d9c94d24cd7bb16be375df62ebd7e00138
Rich <rich.hildebrand@gmail.com>
2013-12-04 20:04:03 -0500

    cleaned up performance fix for drawing map

--- App/Map/HexDrawer.js
+++ App/Map/HexDrawer.js
@@ -9,7 +9,6 @@ define(['Events/HexEvents'], function(hexEvents) {
 			return _hexImageCollection[nodeDetails.groundImage];
 		}
 
-		//else
 		var project = new paper.Project();
 		var layer  = new paper.Layer();
 
@@ -23,48 +22,34 @@ define(['Events/HexEvents'], function(hexEvents) {
 		var hexRaster = new paper.Raster(nodeDetails.groundImage);
 		hexRaster.fitBounds(hexagon.bounds, true);
 
-		var hexImage = layer.rasterize();//.toDataURL();
-		var grrr = new paper.Symbol(hexImage);
+		var saveSymbol = new paper.Symbol(layer);
 
 		layer.remove();
 		project.remove();
 
-		//var img = new Image();
-		//img.src = hexImage;
-
-
-		_hexImageCollection[nodeDetails.groundImage] = grrr;
-		//_hexImageCollection[nodeDetails.groundImage] = img;
+		_hexImageCollection[nodeDetails.groundImage] = saveSymbol;
 
 		return _hexImageCollection[nodeDetails.groundImage];
-	}
-
-	var _drawBottomLayer = function(nodeDetails, hexToCopy, bottomLayer) {
-		bottomLayer.activate();
-
-		var group = new paper.Group();
-
-		var maskingHex = hexToCopy.copyTo(group);
-		maskingHex.setClipMask(true);
-
-		var hexImage = new paper.Raster(nodeDetails.groundImage, hexToCopy.position);
-		hexImage.fitBounds(maskingHex.bounds, true);
-
-		group.addChild(hexImage);
 	}
 
 	var _getHexStartingPosition = function(coordinates) {
 		var size = _mapDetails.hexSize;
 
-		var startingPosition = _(_topLeftPoint).clone();
-		startingPosition.x += Math.round(size._width * (coordinates.column + (coordinates.row % 2 ? 0.5 : 0)));
-		startingPosition.y += Math.round(size._height *(coordinates.row * 0.75));
+		var startingPosition = { x: _topLeftPoint.x, y: _topLeftPoint.y };
+		startingPosition.x += _quickRound(size._width * (coordinates.column + (coordinates.row % 2 ? 0.5 : 0)));
+		startingPosition.y += _quickRound(size._height *(coordinates.row * 0.75));
 
 		return startingPosition;
 	}
 
+	var _quickRound = function(number) {
+		var roundedNumber = (0.5 + number) | 0;
+		roundedNumber = ~~ (0.5 + number);
+		roundedNumber = (0.5 + number) << 0;
+		return roundedNumber;
+	}
+
 	var HexDrawer = function(mapDetails) { 
-		//this will be used to calculate positions before hex is created to avoid bug with setPosition
 		_mapDetails = mapDetails;
 		var hexagon = new paper.Path.RegularPolygon({
 			sides: 6,
@@ -72,12 +57,14 @@ define(['Events/HexEvents'], function(hexEvents) {
 		});
 
 		_topLeftPoint = new paper.Point(mapDetails.hexRadius, mapDetails.hexRadius);
+		_topLeftPoint.x = _quickRound(_topLeftPoint.x);
+		_topLeftPoint.y = _quickRound(_topLeftPoint.y);
+
 		mapDetails.hexSize = hexagon.bounds.size;
 		hexagon.remove();
 	}
 
 	HexDrawer.prototype.drawHex = function(nodeDetails, coordinates, topLayer, bottomLayer) {
-		//calculate position first to avoid bug with setPosition;
 		var hexPosition = _getHexStartingPosition(coordinates);
 		var topLayerHexImage = _drawTopLayer(nodeDetails, coordinates, hexPosition, topLayer);
 
@@ -85,12 +72,6 @@ define(['Events/HexEvents'], function(hexEvents) {
 		topLayer.activate();
 
 		topLayerHexImage.place(hexPosition);
-		//var group = new paper.Group();
-		//group.addChild(topLayerHexImage);
-		//group.setPosition(hexPosition)
-
-		//_drawBottomLayer(nodeDetails, topLayerHex, bottomLayer);
-		//paper.getView()._context.drawImage(topLayerHexImage, hexPosition.x, hexPosition.y);
 
 		return topLayerHexImage;
 	}
