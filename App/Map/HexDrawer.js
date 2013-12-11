36543d65ed557cec4379c8af45a3fefe8b9168a8
Rich <rich.hildebrand@gmail.com>
2013-12-10 23:57:09 -0500

    a map! sort of.

--- App/Map/HexDrawer.js
+++ App/Map/HexDrawer.js
@@ -5,37 +5,32 @@ function(hexPositionCalculator) {
 	var _radius;
 	var _xHexSize;
 	var _yHexSize;
+	var _hexTypes;
 
-	var _drawHex = function(xGrid, yGrid, mapContext) {
-	    var part = 60;
-
-	    var shiftY =0
+	var _drawHex = function(xGrid, yGrid, hexCanvas, mapContext) {
+	    var shiftY = 0;
 		if (xGrid % 2 !== 0) {
-		  var shiftY = yHexSize/2;
+		  var shiftY = _yHexSize / 2;
 		}
 
-		for (var i=0;i<=hexSides;i++) {
-		  var a = i * part - 180;
-		  var x = r * Math.cos(a * Math.PI / 180)+xGrid*xHexSize;
-		  var y = r * Math.sin(a * Math.PI / 180)+yGrid*yHexSize+shiftY;
-		  if (i == 0) {
-		       mapContext.moveTo(x,y);
-		  }
-		  else {
-		      mapContext.lineTo(x,y);
-		  }
-		}
+		//var part = 60;
+		var a = 180; //hexSide * part - 180;
+	    var x = _radius * Math.cos(a * Math.PI / 180) + xGrid * _xHexSize;
+	    var y = _radius * Math.sin(a * Math.PI / 180) + yGrid * _yHexSize + shiftY;
+        //mapContext.moveTo(x,y);
+
+        mapContext.drawImage(hexCanvas, x, y);
 	}
 
 	var _drawSingleHex = function(mapContext) {
 		var part = 60;
 		mapContext.beginPath();
 
-		for (var i=0;i<=_hexSides;i++) {
-		  var a = i * part - 180;
+		for (var hexSide = 0; hexSide <= _hexSides; hexSide++) {
+		  var a = hexSide * part - 180;
 		  var x = _radius * Math.cos(a * Math.PI / 180) + _radius;
 		  var y = _radius * Math.sin(a * Math.PI / 180) + (_yHexSize / 2) + 1;
-		  if (i !== 0) {
+		  if (hexSide !== 0) {
 		      mapContext.lineTo(x,y);
 		  }
 		}
@@ -53,18 +48,39 @@ function(hexPositionCalculator) {
 							 0, 0, radius2x, radius2x);
 	}
 
+	var _createSingleHex = function() {
+		var hexCanvas = document.createElement('canvas');
+		hexCanvas.width = _radius * 2;
+		hexCanvas.height = _radius * 2;
+		var context = hexCanvas.getContext('2d');
+
+		_drawHexImage(context);
+		context.globalCompositeOperation = 'destination-in';
+		_drawSingleHex(context);
+
+		return hexCanvas;
+	}
+
+	var _getImageCanvas = function(nodeType) {
+		var hexCanvas = _hexTypes[nodeType]
+		if (!hexCanvas) {
+			hexCanvas = _createSingleHex();
+		}
+		return hexCanvas;
+	}
+
 	var HexDrawer = function(mapDetails) { 
 	    _hexSides = 6;
 	    _radius = mapDetails.hexRadius;
 	    _xHexSize = _radius*Math.sqrt(2.25);
 	    _yHexSize = _radius*Math.sqrt(3);
+
+	    _hexTypes = [];
 	}
 
 	HexDrawer.prototype.drawHex = function(column, row, mapContext) {
-		//_drawHex(column, row, mapContext);
-		_drawHexImage(mapContext);
-		mapContext.globalCompositeOperation = 'destination-in';
-		_drawSingleHex(mapContext);
+		var hexCanvas = _getImageCanvas('default');
+		_drawHex(column, row, hexCanvas, mapContext);
 	}
 
 	return HexDrawer;
