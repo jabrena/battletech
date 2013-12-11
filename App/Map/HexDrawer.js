f0b978d55392049c7a57bc8b9703de92d0417294
Rich <rich.hildebrand@gmail.com>
2013-12-10 20:40:31 -0500

    spun the hexes

--- App/Map/HexDrawer.js
+++ App/Map/HexDrawer.js
@@ -4,24 +4,22 @@ function(hexPositionCalculator) {
 	var _mapDetails;
 
 	var _drawHex = function(xGrid, yGrid, mapContext) {
+		var hexSides = 6;
+
 	    //length of line
 	    var r = _mapDetails.hexRadius;
 	    var part = 60;
-	    var hexSize = r*Math.sqrt(3);
-	    var yHexSize = r*Math.sqrt(2.25);
+	    var xHexSize = r*Math.sqrt(2.25);
+	    var yHexSize = r*Math.sqrt(3);
 
-		if (yGrid % 2 == 0) {
-		  //even row
-		  var shiftX = hexSize/2;
+	    var shiftY =0
+		if (xGrid % 2 !== 0) {
+		  var shiftY = yHexSize/2;
 		}
-		else {
-		  //odd row
-		  shiftX=0;
-		}
-		for (var i=0;i<=6;i++) {
-		  var a = i * part - 90;
-		  var x = r * Math.cos(a * Math.PI / 180)+xGrid*hexSize+shiftX;
-		  var y = r * Math.sin(a * Math.PI / 180)+yGrid*yHexSize;
+		for (var i=0;i<=hexSides;i++) {
+		  var a = i * part - 180;
+		  var x = r * Math.cos(a * Math.PI / 180)+xGrid*xHexSize;
+		  var y = r * Math.sin(a * Math.PI / 180)+yGrid*yHexSize+shiftY;
 		  if (i == 0) {
 		       mapContext.moveTo(x,y);
 		  }
