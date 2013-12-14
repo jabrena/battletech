1cc84646e41f722d102fc6330da093fdc6d14bea
Rich <rich.hildebrand@gmail.com>
2013-12-13 20:24:58 -0500

    view no longer exits map bounds

--- Spec/UnitTests/ViewPort/getView.spec.js
+++ Spec/UnitTests/ViewPort/getView.spec.js
@@ -1,32 +1,75 @@
-define(['Squire'], function(Squire) {
-	describe('ViewPort', function() {
-		describe('getView', function() {
-			'use strict';
-			var _viewPort;
+define(['Squire', 'AppGlobals'], function(Squire, appGlobals) {
+   describe('ViewPort', function() {
+      describe('getView', function() {
+         'use strict';
+         var _viewPort;
+         var _mapDetails;
 
-			beforeEach(function(done) {
-				var testContext = {};
+         beforeEach(function(done) {
+            var testContext = {};
+            testContext.injector = new Squire();
 
-				testContext.injector = new Squire();
+            appGlobals.camera.bounds.width = 100;
+            appGlobals.camera.bounds.height = 100;
+            testContext.injector.mock('AppGlobals', appGlobals);
 
-				testContext.injector.require(['Map/ViewPort'], function(ViewPort) {
-					testContext.ViewPort = new ViewPort();
-					_viewPort = testContext.ViewPort;
-					done();
-				});
-			});
+            testContext.injector.require(['Map/ViewPort'], function(ViewPort) {
+               _mapDetails = {
+                  hexSize: {
+                     width: 5,
+                     height: 5
+                  },
+                  height: 10,
+                  width: 12
+               }
 
-			describe('When there is smoke', function() {
-				it('should cause red', function() {
-					expect(false).to.be.true;
-				})
-			});
+               testContext.ViewPort = new ViewPort(_mapDetails);
+               _viewPort = testContext.ViewPort;
+               done();
+            });
+         });
 
-			describe('Green means', function() {
-				it('should go.', function() {
-					expect(true).to.be.true;
-				})
-			});
-		});
-	});
+         describe('Given any view coordinates, getView', function() {
+            it('should never set first column less than zero.', function() {
+               appGlobals.camera.view.x = -1000;
+               appGlobals.camera.view.y = 0;
+
+               var view = _viewPort.getView();
+
+               expect(view.firstRow).to.equal(0);
+               expect(view.firstColumn).to.equal(0);
+            });
+
+            it('should never set first row less than zero.', function() {
+               appGlobals.camera.view.x = 0;
+               appGlobals.camera.view.y = -1000;
+
+               var view = _viewPort.getView();
+
+               expect(view.firstRow).to.equal(0);
+               expect(view.firstColumn).to.equal(0);
+            });
+
+            it('should never set last column greater than map width', function() {
+               appGlobals.camera.view.x = 10000;
+               appGlobals.camera.view.y = 0;
+
+               var view = _viewPort.getView();
+
+               expect(view.lastRow).to.be.below(_mapDetails.height + 1);
+               expect(view.lastColumn).to.be.below(_mapDetails.width + 1);
+            });
+
+            it('should never set last row greater than map height', function() {
+               appGlobals.camera.view.x = 0;
+               appGlobals.camera.view.y = 10000;
+
+               var view = _viewPort.getView();
+
+               expect(view.lastRow).to.be.below(_mapDetails.height + 1);
+               expect(view.lastColumn).to.be.below(_mapDetails.width + 1);
+            });
+         });
+      });
+   });
 });
